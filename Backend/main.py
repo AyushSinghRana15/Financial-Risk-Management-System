da330d2066aa033a4a419c63670786aa1f04cc6c
Ayush_Singh <149757886+AyushSinghRana15@users.noreply.github.com>
2026-03-24 14:49:22 +0530

    Resolved merge conflict in main.py

--- Backend/main.py
+++ Backend/main.py
@@ -1,23 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-<<<<<<< HEAD
-#from database import engine, Base
-#import models
-=======
+
 from database import engine, Base
 import models
-from business_risk_api import router as business_router #Adii
 
->>>>>>> 4d68a4c209e4ae533fa11bb053948c5d03e0ed31
-
+from business_risk_api import router as business_router
 from credit_risk_api import router as credit_router
 from market_risk_api import router as market_router
 from operational_risk_api import router as operational_router
 from E_commerce_fraud_risk_api import router as fraud_router
 
-
-# Neon mein tables automatically ban jaayengi
-#Base.metadata.create_all(bind=engine)
+# Create tables (optional)
+# Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -31,13 +25,10 @@ app.add_middleware(
 
 app.include_router(credit_router)
 app.include_router(market_router)
-<<<<<<< HEAD
 app.include_router(operational_router)
 app.include_router(fraud_router)
-=======
-app.include_router(business_router) #Adii
+app.include_router(business_router)
 
->>>>>>> 4d68a4c209e4ae533fa11bb053948c5d03e0ed31
 @app.get("/")
 def root():
     return {"status": "ok", "message": "Financial Risk API running"}
